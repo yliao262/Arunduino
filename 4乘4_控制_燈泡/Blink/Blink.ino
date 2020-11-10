466bd8531bddffd475fc75eebe2b8f4576364822
yliao262 <74228750+yliao262@users.noreply.github.com>
2020-11-10 14:26:56 +0800

    Add files via upload

---
+++ 4乘4_控制_燈泡/Blink/Blink.ino
@@ -0,0 +1,50 @@
+int x=0;
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  digitalWrite(3, HIGH);    
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+}
+
+void loop() {
+
+ 
+  switch (x)
+  {
+    case 0 :
+      digitalWrite(3, HIGH);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);    
+    break;
+    case 1 :
+    digitalWrite(3, LOW);
+    
+    break;
+    case 2 :
+    digitalWrite(4, LOW);
+
+    break;
+    case 3 :
+   digitalWrite(5, LOW);
+
+    break; 
+    case 4 :
+    digitalWrite(6, LOW);
+
+    break; 
+  }
+  if (digitalRead(2)==0)
+  {
+    delay(200);
+    x= x+1;
+    if(x>4)
+    x=0;
+  }
+
+  }          
